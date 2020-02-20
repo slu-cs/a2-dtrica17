eff70dc00a67f3a08208f4d0181a263d8aa91e77
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-19 21:21:33 -0500

    creation step

---
+++ schema.js
@@ -0,0 +1,20 @@
+// Define a schema to represent a registered voter
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of professors (similar to a Java class with instance variables)
+const Voter = new mongoose.Schema({
+  first: String,
+  last: String,
+  zip: Number,
+  history: String
+});
+
+// Speed up queries on all fields
+Voter.index({first: 1});
+Voter.index({last: 1});
+Voter.index({zip: 1});
+Voter.index({history: 1});
+
+// Compile and export this schema
+module.exports = mongoose.model('Voter', Voter);
