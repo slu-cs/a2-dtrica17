90fb82cc4fb033f8943dfa7f7f3d993de7fdccda
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-24 00:05:47 -0500

    test

---
+++ test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Voter = require('./schema');
+connect();
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input:fs.createReadStream('voters.csv')
+});
+const arr = [];
+//this was done using 1
+//const rows = Voter.split('\n');
+//const data = rows.map(b => b.split(','));
+
+file.on('line', function(line){
+    var columns = line.split(',');
+    var voterguy = new Voter({
+    'firstname': columns[0];
+    'lastname': columns[1];
+    'zipcode': columns[2];
+    'history': columns[3];
+  })
+  arr.push(voterguy);
+})
+
+
+
+// Reset the data
+mongoose.connection.dropDatabase()
+  .then(() => arr.save())
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
