6e2cfd3a2a6849ca5684e2bb89abe41fc0e13318
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-24 00:20:05 -0500

    query adjustments

--- query.js
+++ query.js
@@ -9,13 +9,13 @@ connect(); // To the database
 const queries = [
 
   // How many registered voters live in the Canton zip code (13617)?
-  Voter.find().where('zip').equals(13617),
+  Voter.countDocuments().where('zip').equals(13617),
 
   // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('first').equals('STARR'),
 
   // How many people voted in the 2016 general election (GE16)?
-  Voter.find().where('history').in('GE16'),
+  Voter.countDocuments().where('history').in('GE16'),
 
   // What is the last-name that comes last in the county in alphabetical order?
   Voter.find().sort('-last').limit(1),
@@ -31,6 +31,6 @@ Promise.all(queries)
     console.log('All voters with last name of STARR: ', results[1].map(p => p.first));
     console.log('2016 general election voter total: ', results[2]);
     console.log('The last last name: ', results[3].map(p => p.last));
-    console.log('Distinct zip codes: ', results[4]);
+    console.log('Distinct zip codes: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
