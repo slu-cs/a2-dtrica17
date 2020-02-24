ca199afe9d6d5dd7bb33b8e7e6ae88a1988ee767
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-24 09:11:49 -0500

    test

--- create.js
+++ create.js
@@ -25,39 +25,31 @@ file.on('line', function(line) {
   const rows = line.split('\n');
   const data = rows.map(d => d.split(','));
   for (const row in data){
-      list.push(new Voter({
+      const voter = new Voter({
       first: data[0][0],
       last: data[0][1],
       zip: data[0][2],
       history: data[0][3]
-    }));
+    });
     // console.log(voter);
     // console.log(list.length);
-    // list.push(voter);
-    // voter.save();
+    list.push(voter);
+    voter.save();
   }
-  // console.log(list);
-  const saves = list.map(d => d.save());
-  Promise.all(saves)
-    .then(() => console.log('All saved'))
-    .catch(error => console.log(error.stack));
 });
 
-console.log(list);
-
-/*
+/
 const saves = documents.map(d => d.save());
 Promise.all(saves)
   .then(() => console.log('All saved'))
   .catch(error => console.log(error.stack));
-
 
 // Ready database
 mongoose.connection.dropDatabase()
   .then(() => mongoose.connection.close())
   .then(() => console.log('Database is ready.'))
   .catch(error => console.error(error.stack));
-*/
+
 
 
 
@@ -85,3 +77,33 @@ const output = data.map(row => {
 // https://www.npmjs.com/package/csv-parser
 // Mongoose site: https://mongoosejs.com/docs/api/connection.html#connection_Connection-dropDatabase
 // https://stackoverflow.com/questions/48015270/steps-to-create-javascript-objects-from-text-data-out-of-a-csv-file-using-no-lib
+
+
+/*
+const list = [];
+
+// Parse and prepare csv file
+file.on('line', function(line) {
+  const rows = line.split('\n');
+  const data = rows.map(d => d.split(','));
+  for (const row in data){
+      list.push(new Voter({
+      first: data[0][0],
+      last: data[0][1],
+      zip: data[0][2],
+      history: data[0][3]
+    }));
+    // console.log(voter);
+    // console.log(list.length);
+    // list.push(voter);
+    // voter.save();
+  }
+  // console.log(list);
+  const saves = list.map(d => d.save());
+  Promise.all(saves)
+    .then(() => console.log('All saved'))
+    .catch(error => console.log(error.stack));
+});
+
+console.log(list);
+*/
