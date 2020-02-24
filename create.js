f64a614a0718197823506e015e8c6263b62c77bd
Darren Ricalton <dtrica17@stlawu.edu>
2020-02-24 11:30:27 -0500

    update

--- create.js
+++ create.js
@@ -29,12 +29,10 @@ file.on('line', function(line) {
       zip: data[2],
       history: data[3]
   });
-    // console.log(voter);
-    // console.log(list.length);
     list.push(voter.save());
-    console.log(list.length);
-  // console.log('done with creation of voters');
+    console.log(list.length); // 65421 total voters
 });
+
 
 
 file.on('close', function() {
@@ -45,72 +43,8 @@ file.on('close', function() {
 });
 
 
-/*
-const saves = documents.map(d => d.save());
-Promise.all(saves)
-  .then(() => console.log('All saved'))
-  .catch(error => console.log(error.stack));
-
-// Ready database
-mongoose.connection.dropDatabase()
-  .then(() => mongoose.connection.close())
-  .then(() => console.log('Database is ready.'))
-  .catch(error => console.error(error.stack));
-
-
-
-
-/*
-mongoose.connection.dropDatabase();
-
-file.on('close', function()) {
-  console.log('Database is ready.')
-  mongoose.connection.close();
-  process.exit(0)
-}
-
-
-const rows = csv.split('\n');
-const data = rows.map(d => d.split(','));
-console.log(data);
-
-const output = data.map(row => {
-   const obj = {};
-   headers.forEach((h, i) => obj[h] = row[i] });
-   return obj;
-});
-*/
+=
 
 // https://www.npmjs.com/package/csv-parser
 // Mongoose site: https://mongoosejs.com/docs/api/connection.html#connection_Connection-dropDatabase
 // https://stackoverflow.com/questions/48015270/steps-to-create-javascript-objects-from-text-data-out-of-a-csv-file-using-no-lib
-
-
-/*
-const list = [];
-
-// Parse and prepare csv file
-file.on('line', function(line) {
-  const rows = line.split('\n');
-  const data = rows.map(d => d.split(','));
-  for (const row in data){
-      list.push(new Voter({
-      first: data[0][0],
-      last: data[0][1],
-      zip: data[0][2],
-      history: data[0][3]
-    }));
-    // console.log(voter);
-    // console.log(list.length);
-    // list.push(voter);
-    // voter.save();
-  }
-  // console.log(list);
-  const saves = list.map(d => d.save());
-  Promise.all(saves)
-    .then(() => console.log('All saved'))
-    .catch(error => console.log(error.stack));
-});
-
-console.log(list);
-*/
